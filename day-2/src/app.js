01991ddb1509542ba0b70125e3e84a717fb77e5c
der-het <187752127+der-het@users.noreply.github.com>
2026-06-21 11:24:19 +0530

    learn about the 3 api methods implment it

--- day-2/src/app.js
+++ day-2/src/app.js
@@ -1,5 +1,31 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+const note = [];
+
+app.post("/notes", (req, res) => {
+  note.push(req.body);
+
+  res.status("201").json({
+    massage: "resoureces are okay",
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.status("200").json({
+    message: "data reatch",
+    note: note,
+  });
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete note[index];
+  console.log(index);
+  res.status("200").json({
+    message: "node delted sussesfully",
+  });
+});
 
 module.exports = app;
