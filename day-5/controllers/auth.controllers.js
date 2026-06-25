5bb4589f86f6d7a06d263ffe78b64f8097ac1c11
der-het <187752127+der-het@users.noreply.github.com>
2026-06-25 23:02:51 +0530

    understand little jsonwebtoken

--- day-5/controllers/auth.controllers.js
+++ day-5/controllers/auth.controllers.js
@@ -1,7 +1,26 @@
-const userModel = "../src/model/user.model.js";
+const userModel = require("../src/model/user.model");
+const jwt = require("jsonwebtoken");
 
 async function registerUser(req, res) {
-  const { user, email, password } = req.body;
+  const { username, email, password } = req.body; //routes mathi ayaa avshe
+  const user = await userModel.create({
+    username,
+    email,
+    password,
+  }); //user crate karyo
+
+  //json token create here
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+  res.status(201).json({
+    message: "user has registered",
+    user,
+    token,
+  });
 }
 
 module.exports = { registerUser };
--- day-5/src/routes/auth.routes.js
+++ day-5/src/routes/auth.routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const authController = require("./auth.routes");
+const authController = require("../../controllers/auth.controllers.js");
 const router = express.Router();
 
 //post  api created here ee pna register name ni
 router.post("/register", authController.registerUser);
+//app mathi api ni req aya avshe atle apde ane redirect kari didhi
+//haji ek controll naam na folder ma redirect kari didhi
+// je api ni need hoy te aya create thay
+//pachi tena logic ne controllers ma send kari dey
 
-//router ni help thi export kari didhi //5:12:
+//-----------------------------------------------------------------------------------
+//router ni help thi export kari didhi
 module.exports = router;
